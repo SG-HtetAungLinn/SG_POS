2e8306f8dae876f86818c99b7f4630750c75e17a
SG-HtetAungLinn <softguide.htetaunglinn@gmail.com>
2025-06-05 12:04:58 +0630

    first commit

--- admin/register.php
+++ admin/register.php
@@ -3,27 +3,80 @@ require '../require/db.php';
 require '../require/common.php';
 $error = false;
 $error_msg = '';
-$name =
+$name_error =
+    $email_error =
+    $phone_error =
+    $gender_error =
+    $password_error =
+    $confirm_password_error =
+    $name =
     $email =
     $phone =
     $gender =
     $password =
-    $name_error =
     $confirm_password = '';
 
 
 if (isset($_POST['form_sub']) && $_POST['form_sub'] == '1') {
     $name = $mysqli->real_escape_string($_POST['name']);
-    $email = $mysqli->real_escape_string($_POST['name']);
+    $email = $mysqli->real_escape_string($_POST['email']);
     $phone = $mysqli->real_escape_string($_POST['phone']);
     $gender = isset($_POST['gender']) ? $mysqli->real_escape_string($_POST['gender']) : '';
     $password = $mysqli->real_escape_string($_POST['password']);
     $confirm_password = $mysqli->real_escape_string($_POST['confirm_password']);
+    // Name Validation
     if (strlen($name) === 0) {
         $error = true;
         $name_error = "Name is require.";
-    }else if(){
-        
+    } else if (strlen($name) < 3) {
+        $error = true;
+        $name_error = "Name must be less then 3.";
+    } else if (strlen($name) >= 100) {
+        $error = true;
+        $name_error = "Name must be greather then 100.";
+    }
+    // Email Validation
+    if (strlen($email) === 0) {
+        $error = true;
+        $email_error = "Email is require.";
+    } else if (strlen($email) < 3) {
+        $error = true;
+        $email_error = "Email must be less then 3.";
+    } else if (strlen($email) >= 100) {
+        $error = true;
+        $email_error = "Email must be greather then 100.";
+    }
+    // Phone Validation
+    if (strlen($phone) === 0) {
+        $error = true;
+        $phone_error = "Phone is require.";
+    } else if (strlen($phone) < 3) {
+        $error = true;
+        $phone_error = "Phone must be less then 3.";
+    } else if (strlen($phone) >= 50) {
+        $error = true;
+        $phone_error = "Phone must be greather then 50.";
+    }
+    // Gender Validation
+    if (strlen($gender) === 0) {
+        $error = true;
+        $gender_error = "Gender is require.";
+    }
+    // Password Validate
+    if (strlen($password) === 0) {
+        $error = true;
+        $password_error = "Password is require.";
+    } else if (strlen($password) < 8) {
+        $error = true;
+        $password_error = "Password must be less then 8.";
+    } else if (strlen($password) >= 30) {
+        $error = true;
+        $password_error = "Password must be greather then 30.";
+    }
+    // Confirm Password Validation 
+    else if ($password !== $confirm_password) {
+        $error = true;
+        $confirm_password_error = "Password and Confirm Password are not same.";
     }
 }
 ?>
@@ -70,36 +123,51 @@ if (isset($_POST['form_sub']) && $_POST['form_sub'] == '1') {
 
                                 <form class="mt-5 mb-5 login-input" method="POST">
                                     <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Name" name="name">
+                                        <input type="text" class="form-control" placeholder="Name" name="name" value="<?= $name ?>" />
                                         <?php if ($error && $name_error) { ?>
                                             <span class="text-danger"><?= $name_error ?></span>
                                         <?php } ?>
                                     </div>
                                     <div class="form-group">
-                                        <input type="email" class="form-control" placeholder="Email" name="email" />
+                                        <input type="email" class="form-control" placeholder="Email" name="email" value="<?= $email ?>" />
+                                        <?php if ($error && $email_error) { ?>
+                                            <span class="text-danger"><?= $name_error ?></span>
+                                        <?php } ?>
                                     </div>
                                     <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Phone" name="phone" />
+                                        <input type="text" class="form-control" placeholder="Phone" name="phone" value="<?= $phone ?>" />
+                                        <?php if ($error && $phone_error) { ?>
+                                            <span class="text-danger"><?= $phone_error ?></span>
+                                        <?php } ?>
                                     </div>
                                     <div class="form-group">
                                         <div class="form-check">
-                                            <input class="form-check-input" type="radio" value="male" id="male" name="gender" />
+                                            <input class="form-check-input" type="radio" value="male" id="male" name="gender" <?= $gender == 'male' ? 'checked' : '' ?> />
                                             <label class="form-check-label" for="male">
                                                 Male
                                             </label>
                                         </div>
                                         <div class="form-check">
-                                            <input class="form-check-input" type="radio" value="female" id="female" name="gender" />
+                                            <input class="form-check-input" type="radio" value="female" id="female" name="gender" <?= $gender == 'female' ? 'checked' : '' ?> />
                                             <label class="form-check-label" for="female">
                                                 Female
                                             </label>
                                         </div>
+                                        <?php if ($error && $gender_error) { ?>
+                                            <span class="text-danger"><?= $gender_error ?></span>
+                                        <?php } ?>
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" name="password" placeholder="Password" />
+                                        <input type="password" class="form-control" name="password" placeholder="Password" value="<?= $password ?>" />
+                                        <?php if ($error && $password_error) { ?>
+                                            <span class="text-danger"><?= $password_error ?></span>
+                                        <?php } ?>
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" />
+                                        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" value="<?= $confirm_password ?>" />
+                                        <?php if ($error && $confirm_password_error) { ?>
+                                            <span class="text-danger"><?= $confirm_password_error ?></span>
+                                        <?php } ?>
                                     </div>
                                     <input type="hidden" name="form_sub" value="1" />
                                     <button class="btn login-form__btn submit w-100">Sign in</button>
